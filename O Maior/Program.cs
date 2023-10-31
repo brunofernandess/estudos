ab9ea68835030d3827b9947355fe02b240745266
Bruno Fernandes <107273827+brunofernandess@users.noreply.github.com>
2023-10-31 15:28:26 -0300

    Resolução do problema

---
+++ O Maior/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+string[] valores = Console.ReadLine().Split(' ');
+int valor1 = int.Parse(valores[0]);
+int valor2 = int.Parse(valores[1]);
+int valor3 = int.Parse(valores[2]);
+
+int maiorValor = Math.Max(valor1, Math.Max(valor2, valor3));
+
+Console.WriteLine(maiorValor + " eh o maior");
